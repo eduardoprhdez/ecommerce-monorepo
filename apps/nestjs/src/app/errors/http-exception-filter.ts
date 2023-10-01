1aff26efaf56311badb089c32fb7a5423163c5c9
Eduardo Pérez Hernández <eduardo.perez_hernandez@contractors.roche.com>
2023-10-01 20:11:45 +0200

    feat(shared): initial comit shared library

---
+++ apps/nestjs/src/app/errors/http-exception-filter.ts
@@ -0,0 +1,28 @@
+import { BaseError, HttpStatusCode } from '@ecommerce-monorepo/shared';
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    if (host.getType() !== 'http') return;
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof BaseError
+        ? exception.httpCode
+        : HttpStatusCode.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message:
+        exception instanceof BaseError
+          ? exception.message
+          : 'There was an unexpected internal error',
+    });
+  }
+}
