79dfb090db44fad2e696135398634ca63d9598dd
Eduardo Pérez Hernández <eduardo.perez_hernandez@contractors.roche.com>
2023-10-10 12:39:14 +0200

    fix(nestjs): correct stop debezium endpoints

--- apps/nestjs/debezium-connectors/stop-connectors.sh
+++ apps/nestjs/debezium-connectors/stop-connectors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Utiliza curl para crear el conector Debezium
-curl -i -X DELETE -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ecommerce-notifications
-curl -i -X DELETE -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ecommerce-order-placing
+curl -i -X DELETE -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ecommerce-saga-messages
+curl -i -X DELETE -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ecommerce-events
